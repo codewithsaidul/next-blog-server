0afd4f524743c1b1e295e1a3046e4ae7b29201c6
codewithsaidul <codewithsaidul@gmail.com>
2025-09-15 18:37:41 +0600

    searching implemented using prisma

--- src/app/modules/post/post.controller.ts
+++ src/app/modules/post/post.controller.ts
@@ -18,7 +18,8 @@ const getAllPost = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const page  = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
-    const result = await PostService.getAllPost(page, limit);
+    const search = req.query.search || "";
+    const result = await PostService.getAllPost(page, limit, search as string);
 
     res
       .status(200)
--- src/app/modules/post/post.service.ts
+++ src/app/modules/post/post.service.ts
@@ -18,11 +18,27 @@ const createPost = async (payload: Prisma.PostCreateInput): Promise<Post> => {
   return result;
 };
 
-const getAllPost = async (page: number, limit: number) => {
+const getAllPost = async (page: number, limit: number, search: string) => {
   const skip = (page - 1) * limit
   const posts = await prisma.post.findMany({
     skip,
     take: limit,
+    where: {
+      OR: [
+        {
+          title: {
+            contains: search,
+            mode: "insensitive"
+          },
+        },
+        {
+          content: {
+            contains: search,
+            mode: "insensitive"
+          }
+        }
+      ]
+    },
     include: {
       author: {
         select: {
